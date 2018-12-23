70d181b554621232802fe4ed844635c42da8541c
user2116 <42808727+user2116@users.noreply.github.com>
2018-12-24 01:09:56 +0800

    代码实现获取access_token

---
+++ AccessToken.java
@@ -0,0 +1,20 @@
+public class AccessToken {
+    private String tokenName;
+    private int expireSecond;    
+
+    public String getTokenName() {
+        return tokenName;
+    }
+
+    public void setTokenName(String tokenName) {
+        this.tokenName = tokenName;
+    }
+
+    public int getExpireSecond() {
+        return expireSecond;
+    }
+
+    public void setExpireSecond(int expireSecond) {
+        this.expireSecond = expireSecond;
+    }
+}
